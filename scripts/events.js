a05382e0ad5669099bb034abffd9c90db47af720
laichurui <sedcshyzx@gmail.com>
2020-08-16 20:14:27 +0800

    v 0.2.0 鼠标拖拽调整大小

--- scripts/events.js
+++ scripts/events.js
@@ -11,3 +11,54 @@ function switchState() {
         state == null || state === "default" ? "fit" : "default"
     );
 }
+
+/**
+ * 鼠标按下事件
+ */
+function resizeBarDown() {
+    this.classList.add("_clicked");
+
+    //取消过渡动画
+    let wrapper = document.querySelector(".wrapper")
+    wrapper.classList.add("no-transition");
+}
+
+/**
+ * 鼠标抬起事件
+ */
+function resizeBarUp() {
+    this.classList.remove("_clicked");
+
+    //显示过渡动画
+    let wrapper = document.querySelector(".wrapper")
+    wrapper.classList.remove("no-transition");
+}
+
+/**
+ * 鼠标移动事件
+ * @param event 鼠标事件
+ */
+function resizeBarMove(event) {
+    if (this.classList.contains("_clicked")) {
+        document.documentElement.style.setProperty(
+            "--sidebar-width", `${event.clientX}px`
+        );
+    }
+}
+
+/**
+ * 注册所有事件
+ */
+function registerEvent() {
+    /** @type {HTMLElement} */
+    let test = document.getElementById("test");
+    test.addEventListener("click", switchState);
+
+    /** @type {HTMLElement} */
+    let resizeBar = document.querySelector(".resize-bar");
+    resizeBar.addEventListener("mousedown", resizeBarDown);
+    resizeBar.addEventListener("mouseup", resizeBarUp);
+    resizeBar.addEventListener("mousemove", resizeBarMove);
+}
+
+window.addEventListener("load", registerEvent);
