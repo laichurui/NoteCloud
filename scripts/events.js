ff4cd686e8025a0b454a5b98e6340eccfab96802
laichurui <sedcshyzx@gmail.com>
2020-08-20 18:21:47 +0800

    v 0.3.8 优化侧边栏放置目录

--- scripts/events.js
+++ scripts/events.js
@@ -1,79 +1,50 @@
-/**
- * 切换页面状态<br>
- */
-function switchState() {
-    /** @type {HTMLElement} */
-    let wrapper = document.querySelector(".wrapper");
-    /** @type {string} */
-    let state = wrapper.getAttribute("data-state");
-    wrapper.setAttribute(
-        "data-state",
-        state == null || state === "default" ? "fit" : "default"
-    );
-}
+// 页面加载后注册标签事件
+window.addEventListener("load", function () {
 
-
-/**
- * 鼠标按下事件
- */
-function resizeBarDown() {
-    this.classList.add("_clicked");
-
-    //取消过渡动画
-    let wrapper = document.querySelector(".wrapper")
-    wrapper.classList.add("no-transition");
-}
-
-/**
- * 鼠标抬起事件
- */
-function resizeBarUp() {
-    this.classList.remove("_clicked");
-
-    //显示过渡动画
-    let wrapper = document.querySelector(".wrapper")
-    wrapper.classList.remove("no-transition");
-}
-
-/**
- * 鼠标移动事件
- * @param event 鼠标事件
- */
-function resizeBarMove(event) {
-    if (this.classList.contains("_clicked")) {
-        document.documentElement.style.setProperty(
-            "--sidebar-width", `${event.clientX}px`
+    // 打开或关闭侧边栏
+    document.getElementById("btn_switch_wrapper_state").addEventListener("click", () => {
+        let wrapper = document.querySelector(".wrapper");
+        let state = wrapper.getAttribute("data-state");
+        wrapper.setAttribute(
+            "data-state",
+            state == null || state === "default" ? "fit" : "default"
         );
+    });
+
+    // 设置目录/笔记导航是否显示
+    function changeVisible() {
+        document.querySelector(".sidebar").setAttribute(
+            "data-visible",
+            this.getAttribute("data-arg"));
     }
-}
+    // 侧边栏显示目录
+    let c = document.querySelector(".btn-show-catalogue");
+    c.addEventListener("click", changeVisible);
+    c.addEventListener("mouseover", changeVisible);
+    // 侧边栏显示笔记导航
+    c=document.querySelector(".btn-show-notes");
+    c.addEventListener("click", changeVisible);
+    c.addEventListener("mouseover", changeVisible);
 
-/**
- * 注册所有事件
- */
-function registerEvent() {
-    /** @type {HTMLElement} */
-    let test = document.getElementById("test");
-    test.addEventListener("click", switchState);
-
-    document.querySelector(".btn-show-catalogue").addEventListener(
-        "click", () => {
-            document.querySelector(".sidebar").setAttribute(
-                "data-visible", "catalogue");
-        }
-    );
-
-    document.querySelector(".btn-show-notes").addEventListener(
-        "click", () => {
-            document.querySelector(".sidebar").setAttribute(
-                "data-visible", "notes");
-        }
-    );
-
+    // 拖拽调整侧边栏大小
     /** @type {HTMLElement} */
     let resizeBar = document.querySelector(".resize-bar");
-    resizeBar.addEventListener("mousedown", resizeBarDown);
-    resizeBar.addEventListener("mouseup", resizeBarUp);
-    resizeBar.addEventListener("mousemove", resizeBarMove);
-}
+    resizeBar.addEventListener("mousedown", function () {
+        this.classList.add("_clicked");
+        //取消过渡动画
+        document.querySelector(".wrapper").classList.add("no-transition");
+    });
+    resizeBar.addEventListener("mouseup", function () {
+        this.classList.remove("_clicked");
+        //显示过渡动画
+        document.querySelector(".wrapper").classList.remove("no-transition");
+    });
+    resizeBar.addEventListener("mousemove", function (event) {
+        if (this.classList.contains("_clicked")) {
+            document.documentElement.style.setProperty(
+                "--sidebar-width", `${event.clientX}px`
+            );
+        }
+    });
 
-window.addEventListener("load", registerEvent);
+});
